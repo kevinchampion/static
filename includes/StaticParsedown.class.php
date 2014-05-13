a1b74b6d8c814854f3021538160b0623eaca257f
Kevin Champion <kevin.champion@gmail.com>
2014-05-13 13:05:25 -0400

    Fix problems found with coder and codesniffer. Clean docs.

--- includes/StaticParsedown.class.php
+++ includes/StaticParsedown.class.php
@@ -1,46 +1,61 @@
 <?php
 /**
  * @file
+ * Extends Parsedown class for Static specific modifications.
  */
 
 
 class StaticParsedown extends Parsedown {
 
-  var $base_path;
+  public $basePath;
 
   /**
-   * Defines the base_path for use in
+   * Defines the base_path for use in the reference block.
+   *
+   * Allows definition of specific root directories for asset paths to be found
+   * in.
+   *
+   * @param string $path
+   *   Base path of the asset root.
    */
-  function set_base_path($path) {
+  public function setBasePath($path) {
 
-    $this->base_path = $path;
+    $this->basePath = $path;
 
   }
 
   /**
-   * Override parent reference to inject base_path.
-   * @param [type] $Line [description]
-   * @return [type]
+   * Override parent reference to inject basePath.
+   *
+   * This allows for using the image reference convention in markdown to add
+   * images whose paths will be relative to the Drupal base_path. Without this,
+   * image paths are incorrect.
+   *
+   * @param string $line
+   *   A string line as read from the source file.
+   *
+   * @return array
+   *   A reference block.
    */
-  protected function identifyReference($Line) {
+  protected function identifyReference($line) {
 
-    if (preg_match('/^\[(.+?)\]:[ ]*<?(\S+?)>?(?:[ ]+["\'(](.+)["\')])?[ ]*$/', $Line['text'], $matches)) {
+    if (preg_match('/^\[(.+?)\]:[ ]*<?(\S+?)>?(?:[ ]+["\'(](.+)["\')])?[ ]*$/', $line['text'], $matches)) {
 
       $label = strtolower($matches[1]);
 
       $this->references[$label] = array(
-        'url' => $this->base_path . $matches[2],
+        'url' => $this->basePath . $matches[2],
       );
 
       if (isset($matches[3])) {
         $this->references[$label]['title'] = $matches[3];
       }
 
-      $Block = array(
-        'element' => null,
+      $block = array(
+        'element' => NULL,
       );
 
-      return $Block;
+      return $block;
 
     }
 
